64d4278157b9715d9bdbde5304bd154d518624f8
techking11 <nma1132000@gmail.com>
2025-03-19 23:31:25 +0700

    code refactor and update

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,10 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import Stats from './Stats';
+import useRecentBookings from './useRecentBookings';
+import useRecentStays from './useRecentStays';
+import useCabins from '../cabins/useCabins';
+import Spinner from '../../ui/Spinner';
+import TodayActivity from '../check-in-out/TodayActivity';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +12,23 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+export default function DashboardLayout() {
+  const { isLoading: isLoading1, bookings } = useRecentBookings();
+  const { isLoading: isLoading2, confirmedStays, numDays } = useRecentStays();
+  const { isLoading: isLoading3, cabins } = useCabins();
+
+  if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins?.length}
+      />
+      <TodayActivity />
+    </StyledDashboardLayout>
+  );
+}
